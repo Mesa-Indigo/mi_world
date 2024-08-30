e196df5bac339aa474da66db52ee82c966d95b7e
Agimir <william@mesaindigo.com>
2024-08-30 10:43:16 -0500

    vendor list loading

--- client/data/vendors.lua
+++ client/data/vendors.lua
@@ -3,12 +3,52 @@
 
 
 -- functions
+local loadped = function()
 
+end
+
+local checkinv = function(item, data)
+    local count = Inventory:Search('count', item)
+    if count == 0 then
+        return
+    else
+        lib.callback.await('mi:vend:payout', cache.ped, item, count, data)
+    end
+end
+
+local loadmenu = function(data)
+    local options = {}
+    for k, v in pairs(data.items) do
+        options[#options+1] = {
+            title = v.label,
+            description = 'Item Value: '..v.value,
+            onSelect = function()
+                checkinv('pc_sativa', v)
+            end,
+        }
+    end
+    lib.registerContext({
+        id = data.icon,
+        title = data.label,
+        options = options
+    })
+    lib.showContext(data.icon)
+end
+
+RegisterCommand('vend', function()
+    loadmenu(Data.Vend.recycle)
+end, false)
+
+local options = {
+    {
+
+    }
+}
 
 -- event
 RegisterNetEvent('miwd:c:load:vendors')
 AddEventHandler('miwd:c:load:vendors', function()
     if Shared.Vend then
-        
+
     end
 end)
--- data/vendors.lua
+++ data/vendors.lua
@@ -2,23 +2,25 @@
 Data.Vend = {
     ----------------------------------------------------------------
     ----------------------------------------------------------------
-    -- cannabis vendor
-    {
+    -- recycling vendor
+    recycle = {
         -- blip active / sprite / color / scale
         blip = true, sprite = 5, color = 1, scale = 0.6,
+        -- menu details
+        label = 'Recycling', icon = 'recycle',
         -- location of vendor
         spawn = vec4(-319.000, -1546.691, 27.774, 338.543),
         -- model of vendor / animaiton
         model = 's_m_m_dockwork_01', scen = 'WORLD_HUMAN_CLIPBOARD_FACILITY',
         -- items to be sold
         items = {
-            ['scrap_iron'] = 2,
-            ['scrap_aluminium'] = 2,
-            ['scrap_wood'] = 2,
-            ['scrap_plastic'] = 2,
-            ['scrap_bottle'] = 2,
-            ['scrap_wires'] = 2,
-            ['scrap_bolts'] = 2,
+            ['scrap_iron'] =        { label = 'Iron Scraps',        value = 2 },
+            ['scrap_aluminium'] =   { label = 'Aluminium Scraps',   value = 2 },
+            ['scrap_wood'] =        { label = 'Wood Planks',        value = 2 },
+            ['scrap_plastic'] =     { label = 'Plastic Scraps',     value = 2 },
+            ['scrap_bottle'] =      { label = 'Glass Bottle',       value = 2 },
+            ['scrap_wires'] =       { label = 'Torn Wires',         value = 2 },
+            ['scrap_bolts'] =       { label = 'Bolts & Fittings',   value = 2 },
         },
     }
 }
